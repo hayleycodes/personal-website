ddb47ee07e3025eb8852be236c6066267c9f7ed5
Hayley van Waas <hayleyavw@gmail.com>
2014-09-05 12:02:31 +1200

    WIP initialises database

--- flaskr.py
+++ flaskr.py
@@ -1,7 +1,7 @@
 # all the imports
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -17,6 +17,13 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 #connect to database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+#initialises database
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 #runs server
 if __name__ == '__main__':
