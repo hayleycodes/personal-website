7fed9425e3ebc2af85eb1949d4637d7b4208af8f
Hayley van Waas <hayleyavw@gmail.com>
2014-09-05 20:43:22 +1200

    created config.py file

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,10 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from contextlib import closing
 
-# configuration
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
 
 # create our little application :)
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_object('app.config')
 
 #connect to database
 def connect_db():
@@ -36,9 +29,5 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
-
-
-
-#runs server
-if __name__ == '__main__':
-    app.run()
+from app.core.views import mod as core
+app.register_blueprint(core)
