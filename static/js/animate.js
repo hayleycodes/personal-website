9b0c680d4a821cd3a3ba5d7ef20557e29a9b3c3c
Hayley van Waas <hayleyavw@gmail.com>
2015-02-12 21:13:18 +1300

    changing appearance of cirlces in timeline

--- static/js/animate.js
+++ static/js/animate.js
@@ -6,6 +6,6 @@ $(document).ready(function(){
 	var colors = ["#2ECCFA","#CCCCCC","#F841E3","#18DA12", "#EEEE19", "#A337F0", "#EE2A4B", "#424146", "#FF8000"];           
 	var rand = Math.floor(Math.random()*colors.length);           
 	$('#controls-wrapper').css("background-color", colors[rand]);
-	$('html, .circle').css("background", colors[rand]);
+	$('html, .circle, .cd-timeline-img.cd-picture').css("background", colors[rand]);
 	$('h2, h3, h5, section a, .ss-form-title').css("color", colors[rand]);
 });
