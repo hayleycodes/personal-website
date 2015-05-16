0af29ce563b336943db947e18a060911a1af8bdd
Hayley van Waas <hayleyavw@gmail.com>
2015-05-17 00:28:47 +1200

    touching home page for foundation, bringing back db

--- flaskr.py
+++ flaskr.py
@@ -1,7 +1,7 @@
 #all the imports
-#import sqlite3
+import sqlite3
+from sqlite3 import dbapi2 as sqlite3
 
-#from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from contextlib import closing
 
@@ -10,36 +10,33 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS')
 
-#connect to database
-#def connect_db():
-#    rv = sqlite3.connect(app.config['DATABASE'])
-#    rv.row_factory = sqlite3.Row
-#    return rv
-#    #return sqlite3.connect(app.config['DATABASE'])
+connect to database
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+    #return sqlite3.connect(app.config['DATABASE'])
+initialises database
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
-#initialises database
-#def init_db():
-#    with closing(connect_db()) as db:
-#        with app.open_resource('schema.sql', mode='r') as f:
-#            db.cursor().executescript(f.read())
-#        db.commit()
-#
-#def get_db():
-#    if not hasattr(g, 'sqlite_db'):
-#        g.sqlite_db = connect_db()
-#    return g.sqlite_db
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+runs before each request
+@app.before_request
+def before_request():
+    g.db = connect_db()
 
-
-#runs before each request
-#@app.before_request
-#def before_request():
-#    g.db = connect_db()
-#
-#@app.teardown_request
-#def teardown_request(exception):
-#    db = getattr(g, 'db', None)
-#    if db is not None:
-#        db.close()
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
 
 
 @app.errorhandler(404)
@@ -60,7 +57,7 @@ def show_entries():
     db = get_db()
     cur = db.execute('SELECT * FROM entries ORDER BY id DESC')
     entries = cur.fetchall()
-    return render_template('blog.html', entries=entries)
+    return render_template('articles.html', entries=entries)
 
 @app.route('/projects')
 def projects():
@@ -72,13 +69,6 @@ def NZ():
     return render_template('3DNZ.html')
 
 
-@app.route('/presentations')
-def presentations():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return render_template('presentations.html')
-
 @app.route('/articles')
 def articles():
     return render_template('articles.html')
@@ -88,9 +78,11 @@ def articles():
 def timeline():
     return render_template('timeline.html')
 
+
 @app.route('/contactform')
 def contactform():
     return render_template('contactform.html')
+
 
 @app.route('/viewpost/<postID>')
 def viewpost(postID):
@@ -98,6 +90,7 @@ def viewpost(postID):
     cur = db.execute('SELECT id, title, text FROM entries WHERE id = ?', [postID])
     post = cur.fetchone()
     return render_template('viewpost.html', post=post)
+
 
 #adds users entered data to database
 @app.route('/add', methods=['POST'])
@@ -109,6 +102,7 @@ def add_entry():
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
+
 
 @app.route('/edit', methods=['POST'])
 def edit_entry():
@@ -122,6 +116,7 @@ def edit_entry():
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
+
 @app.route('/delete/<postID>', methods=['POST'])
 def delete_entry(postID):
     if not session.get('logged_in'):
@@ -131,6 +126,7 @@ def delete_entry(postID):
     flash('Entry was deleted')
     return redirect(url_for('show_entries'))
     #return render_template('blog.html')
+
 
 #logs in users
 @app.route('/login', methods=['GET', 'POST'])
@@ -144,9 +140,9 @@ def login():
         else:
             session['logged_in'] = True
             flash('You were logged in')
-            #return redirect(url_for('show_entries'))
-            return redirect(url_for('presentations'))
+            return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
+
 
 #logs out user
 @app.route('/logout')
