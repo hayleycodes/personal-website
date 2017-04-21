fe48d0507a7f77d87ba8a9dd37fc6bf0cd388290
Hayley van Waas <hayleyavw@gmail.com>
2017-04-22 11:45:54 +1200

    fixed failed errors

--- flaskr.py
+++ flaskr.py
@@ -1,11 +1,8 @@
-import os
 from flask import Flask, render_template
 
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
-port = int(os.environ.get('PORT', 33507))
-app.run(host='0.0.0.0', port=port)
 # app.config.from_envvar('FLASKR_SETTINGS')
 
 
