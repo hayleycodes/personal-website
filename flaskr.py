e86d8e15c160c4d32f5138efe6e914ea97554c8b
Hayley van Waas <hayleyavw@gmail.com>
2017-04-22 18:18:41 +1200

    messing with port settings;

--- flaskr.py
+++ flaskr.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 # create our little application :)
 app = Flask(__name__)
@@ -26,4 +27,6 @@ def contact():
 
 # runs server
 if __name__ == '__main__':
-    app.run()
+	port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run()
