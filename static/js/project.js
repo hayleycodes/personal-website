5aefad89731bc0a401cb960076a45102502f8752
Hayley van Waas <hayleyavw@gmail.com>
2019-10-25 19:04:03 +0800

    added all projects and set up job title and date elements

--- static/js/project.js
+++ static/js/project.js
@@ -6,15 +6,15 @@
         constructor() {
             super();
             const projectName = this.projectName;
-            const projectImg = projectName.toLowerCase().replace(/ /g, "-") + '.png'
-
+            const projectImg = projectName.toLowerCase().replace(/ /g, "-").replace(/'/g, "") + '.png'
             const projectLanguages = this.languages;
-            console.log(projectLanguages)
+            const jobTitle = this.jobTitle;
 
             const template = document.createElement('details')
             template.innerHTML = `
             <style>
                 details {
+                    font-family: 'Roboto', sans-serif;
                     margin-bottom: 10px;
                     padding: 10px;
                     background-color: #FFF;
@@ -25,29 +25,46 @@
                     display: flex;
                     align-items: center;
                 }
+
+                details summary .summary-wrapper {
+                    width: 100%;
+                }
                 
-                details summary .summary-content {
+                .summary-row-1, .summary-row-2 {
                     display: flex;
                     justify-content: space-between;
                     align-items: center;
-                    width: 100%;
                 }
+
                 details img {
                     height: 20px;
                 }
+                
+                h4 {
+                    margin: 3px 0 0 0;
+                    color: #3E3E3E;
+                    font-size: 0.8rem;
+                }
+                
             </style>
             
             <summary>
-                <div class="summary-content">
-                    <img src="images/project-logos/${projectImg}" alt="Logo for ${projectName}">
-                    <div id="icons">
-                        ${projectLanguages.map(language => `
-                            <img src="/images/language-logos/${language}.png">
-                        `).join('')}
+                <div class="summary-wrapper">
+                    <div class="summary-row-1">
+                        <img src="images/project-logos/${projectImg}" alt="Logo for ${projectName}">
+                        <div class="icons">
+                            ${projectLanguages.map(language => `
+                                <img src="/images/language-logos/${language}.png">
+                            `).join('')}
+                        </div>
+                    </div>
+                    <div class="summary-row-2">
+                        <h4>${jobTitle}</h4>
+                        <h4>16/12/18 - 16/12/18 (8 months)</h4>
                     </div>
                 </div>
             </summary>
-            <p>Further information</p>
+            <slot class="description" name="project-description"></slot>
             `
             
             const shadow = this.attachShadow({ mode: 'open' });
@@ -58,8 +75,12 @@
             return this.getAttribute('project-name') || '';
         }
 
+        get jobTitle() {
+            return this.getAttribute('job-title') || '';
+        }
+
         get languages() {
-            const allLanguages = ['html', 'css', 'js', 'ts', 'react', 'python', 'django', 'flask']
+            const allLanguages = ['html', 'css', 'js', 'ts', 'threejs', 'react', 'python', 'django', 'flask', 'tornado']
             const relatedLanguages = []
             allLanguages.forEach((language) => {
                 if (this.getAttribute(language) !== null) {
@@ -74,26 +95,3 @@
     customElements.define('project-details', ProjectDetails);
 
 })();
-
-    // // creating a container for the project-details component
-    // const projectDetailsContainer = document.createElement('details');
-
-    // // get attribute values from getters
-
-    // const projectName = this.getAttribute('project-name')
-    // const projectLogo = `<img src="/images/project-logos/${projectName}.png">`
-
-    // const htmlLogo = this.getAttribute('html-logo') === 'true' ? `<img src="/images/language-logos/html.png">` : '';
-    // const cssLogo = this.getAttribute('css-logo') === 'true' ? `<img src="/images/language-logos/css.png">` : '';
-    // const jsLogo = this.getAttribute('js-logo') === 'true' ? `<img src="/images/language-logos/js.png">` : '';
-    // const tsLogo = this.getAttribute('ts-logo') === 'true' ? `<img src="/images/language-logos/ts.png">` : '';
-    // const reactLogo = this.getAttribute('react-logo') === 'true' ? `<img src="/images/language-logos/react.png">` : '';
-    // const pythonLogo = this.getAttribute('python-logo') === 'true' ? `<img src="/images/language-logos/python.png">` : '';
-    // const djangoLogo = this.getAttribute('django-logo') === 'true' ? `<img src="/images/language-logos/django.png">` : '';
-    // const flaskLogo = this.getAttribute('flask-logo') === 'true' ? `<img src="/images/language-logos/flask.png">` : '';
-
-    // adding a class to our container for the sake of clarity
-    // projectDetailsContainer.classList.add('project-details-box');
-
-    // creating the inner HTML of the editable list element
-    // projectDetailsContainer.innerHTML = ``
