88428e1552e45db715a8521393a85b4a402aa85f
Hayley van Waas <hayleyavw@gmail.com>
2019-10-27 13:29:48 +0800

    moved styles into external stylesheet

--- static/js/project.js
+++ static/js/project.js
@@ -16,38 +16,7 @@
             const template = document.createElement('details')
             template.innerHTML = `
             <style>
-                details {
-                    font-family: 'Roboto', sans-serif;
-                    margin-bottom: 10px;
-                    padding: 10px;
-                    background-color: #FFF;
-                    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
-                }
-                
-                details summary {
-                    display: flex;
-                    align-items: center;
-                }
 
-                details summary .summary-wrapper {
-                    width: 100%;
-                }
-                
-                .summary-row-1, .summary-row-2 {
-                    display: flex;
-                    justify-content: space-between;
-                    align-items: center;
-                }
-
-                details img {
-                    height: 20px;
-                }
-                
-                h4 {
-                    margin: 3px 0 0 0;
-                    color: #3E3E3E;
-                    font-size: 0.8rem;
-                }
                 
             </style>
             
@@ -78,6 +47,13 @@
             `
             
             const shadow = this.attachShadow({ mode: 'open' });
+
+            // apply stylesheet
+            const linkElem = document.createElement('link');
+            linkElem.setAttribute('rel', 'stylesheet');
+            linkElem.setAttribute('href', 'stylesheets/style.css');
+
+            shadow.appendChild(linkElem);
             shadow.appendChild(template.cloneNode(true));
         }
             
