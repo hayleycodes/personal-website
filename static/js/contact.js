5bf975291d5e782109471440a3bb20baa748602d
Hayley van Waas <hayleyavw@gmail.com>
2019-11-18 22:44:09 +0800

    WIP improving the contact form - added overlay message

--- static/js/contact.js
+++ static/js/contact.js
@@ -13,6 +13,14 @@ function openContact() {
 // Close when someone clicks on the "x" symbol inside the overlay
 function closeContact() {
     document.getElementById('contact-overlay').style.height = '0%'
+    nameElement.disabled = false
+    emailElement.disabled = false
+    messageElement.disabled = false
+
+    const formElements = document.getElementsByClassName('form-element')
+    for (var i = 0; i < formElements.length; i++) {
+        formElements[i].classList.remove('disabled')
+    }
 }
 
 // Disable the form inputs
@@ -67,18 +75,19 @@ function sendEmail() {
         xhr.open('POST', '/', true)
         xhr.setRequestHeader('Content-Type', 'application/json')
         xhr.onreadystatechange = function() {
+            // disableForm()
             if (xhr.readyState == 4 && xhr.status == 200) {
                 // everything is fine - show validation message
                 console.log('all is well')
                 nameElement.value = ''
                 emailElement.value = ''
                 messageElement.value = ''
-                document.getElementById('send-email-button').value =
-                    'Thanks! Your message has been sent.'
-                disableForm()
+                let overlayMessage = document.getElementById('message-confirmation-overlay')
+                overlayMessage.classList.toggle('show')
                 setTimeout(() => {
+                    overlayMessage.classList.toggle('show')
                     closeContact()
-                }, 2000)
+                }, 3000)
             } else if (xhr.readyState == 4 && xhr.status == 400) {
                 emailElement.style.borderColor = 'red'
                 // invalid email - show error
